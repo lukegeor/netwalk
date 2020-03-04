1faeb266ba46bb366abba5137361fa9c7f701fcf
Luke Georgalas <lgeorgalas@osisoft.com>
2020-03-03 23:40:45 -0800

    Added plugins folder

--- Netwalk-UI/Assets/Scripts/BackgroundScript.cs
+++ Netwalk-UI/Assets/Scripts/BackgroundScript.cs
@@ -6,33 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class BackgroundScript : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        var transformObj = gameObject.transform;
-
-        var screenWidth = Screen.width;
-        var screenHeight = Screen.height;
-        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        var currentImageSize = spriteRenderer.sprite.rect.size.y;
-
-        var margin = 0.02f;
-
-        var newImageSize = screenWidth * (1.0f - 2.0f * margin);
-        var newY = screenHeight / 2.0f - margin * newImageSize - newImageSize / 2.0f;
-
-        var scale = newImageSize / currentImageSize;
-        
-        Debug.Log($"Screen size is {screenWidth}, {screenHeight}");
-        Debug.Log($"Old image size was {currentImageSize}");
-        Debug.Log($"New image size is {newImageSize}");
-        Debug.Log($"Scale is {scale}");
-        Debug.Log($"New position is 0.0, {newY}");
-        
-        //transformObj.localScale = new Vector3(.5f, .5f, 1.0f);
-        transformObj.localPosition = new Vector3(0.0f, 3f, 0.0f);
-        //transformObj.localPosition = new Vector3(0.0f, newY, 0.0f);
-        transformObj.localScale = new Vector3(scale, scale, 1.0f);
+        // var transformObj = gameObject.transform;
+        //
+        // var screenWidth = Screen.width;
+        // var screenHeight = Screen.height;
+        // var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        // var currentImageSize = spriteRenderer.sprite.rect.size.y;
+        //
+        // var margin = 0.02f;
+        //
+        // var newImageSize = screenWidth * (1.0f - 2.0f * margin);
+        // var newY = screenHeight / 2.0f - margin * newImageSize - newImageSize / 2.0f;
+        //
+        // var scale = newImageSize / currentImageSize;
+        //
+        // Debug.Log($"Screen size is {screenWidth}, {screenHeight}");
+        // Debug.Log($"Old image size was {currentImageSize}");
+        // Debug.Log($"New image size is {newImageSize}");
+        // Debug.Log($"Scale is {scale}");
+        // Debug.Log($"New position is 0.0, {newY}");
+        //
+        // //transformObj.localScale = new Vector3(.5f, .5f, 1.0f);
+        // transformObj.localPosition = new Vector3(0.0f, 3f, 0.0f);
+        // //transformObj.localPosition = new Vector3(0.0f, newY, 0.0f);
+        // transformObj.localScale = new Vector3(scale, scale, 1.0f);
     }
 
     // Update is called once per frame
