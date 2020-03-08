168f3dda197eda966839e0c7930daf0a2249a022
Luke Georgalas <lukegeor@gmail.com>
2020-03-08 00:14:06 -0800

    [NW-15] assets (#14)
    * First unity check-in
    
    * Added more sprites
    
    * playing with unity
    
    * scene changed
    
    * Added plugins folder
    
    * Got background working
    
    * Board generates and displays!
    
    * Missed some files
    
    * Removed unused usings.
    
    * Deleted dlls from Assets
    
    * Added Asset dlls to .gitignore
    
    Co-authored-by: Luke Georgalas <lgeorgalas@illumina.com>

---
+++ NetwalkLib/Game/IGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetwalkLib
+{
+    public interface IGame
+    {
+        DateTime StartTime { get; }
+        Board SolvedBoard { get; }
+        Board PlayingBoard { get; }
+        event EventHandler<GameWonEventArgs> GameWonEvent;
+        void RotateCell(int row, int col);
+    }
+}
