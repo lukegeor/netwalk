4ceecefa506da749befebd083b7fe5d54435a422
Luke Georgalas <lukegeor@gmail.com>
2020-03-14 00:53:14 -0700

    [NW-24] add android stuff (#18)
    * First pass at android stuff
    
    * Added 0.3 apk
    
    * Added app bundle
    
    * Added app bundle to gitignore for ui project only
    
    * Changed target platform to include 64 bit, release 0.3
    
    Co-authored-by: Luke Georgalas <lgeorgalas@illumina.com>

--- Netwalk-UI/Assets/Scripts/TileBackgroundScript.cs
+++ Netwalk-UI/Assets/Scripts/TileBackgroundScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using NetwalkLib;
-using TMPro.EditorUtilities;
 using UnityEngine.UI;
 
 public class TileBackgroundScript : MonoBehaviour
